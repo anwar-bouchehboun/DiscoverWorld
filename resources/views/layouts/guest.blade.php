3ca9e72c8260fe47d8b83917df1574eba821c027
anouar <anouar.ab95@gmail.com>
2024-01-30 16:12:17 +0100

    fin dsin page home

---
+++ resources/views/layouts/guest.blade.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css'>
+        <title>{{ config('app.name', 'Laravel') }}</title>
+        <script src="https://cdn.tailwindcss.com"></script>
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+<style>
+    .social-container {
+	margin: 20px 0;
+}
+
+.social-container a {
+	border: 1px solid #DDDDDD;
+	border-radius: 50%;
+	display: inline-flex;
+	justify-content: center;
+	align-items: center;
+	margin: 0 5px;
+	height: 40px;
+	width: 40px;
+}
+</style>
+        <!-- Scripts -->
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    </head>
+    <body class="font-sans antialiased text-gray-900">
+        <div class="flex flex-col items-center min-h-screen pt-6 bg-gray-100 sm:justify-center sm:pt-0">
+            <div class="">
+                <a href="/">
+                    <x-application-logo class="text-gray-500 fill-current" />
+                </a>
+            </div>
+            <div class="flex flex-col justify-center w-full mb-2 sm:flex-row ">
+
+                <div class="bg-sky-800 w-full sm:max-w-md  px-6 py-[10%] shadow-md ">
+                    <h1 class="pb-2 text-5xl text-center text-white bold font-weight-bold"><strong> Welcome </strong></h1>
+                    <div class="text-center social-container">
+                        <a href="#" class=" social"><i class="text-white fab fa-facebook-f"></i></a>
+                        <a href="#" class=" social"><i class="text-white fab fa-google-plus-g"></i></a>
+                        <a href="#" class=" social"><i class="text-white fab fa-linkedin-in"></i></a>
+                    </div>
+                <p class="font-serif text-center text-white text-1xl">To keep connected with us please login with your personal info</p>
+                <a href="{{url('/')}}" class="text-xl font-bold text-center ml-[45%] border-l-4 border-b-4 px-3 text-white border-neutral-300">Home</a>
+
+
+                </div>
+
+                <div class="w-full sm:max-w-md  px-6 py-[4%] bg-white shadow-md overflow-hidden">
+                    <!-- Content for the second column (white background) -->
+                    {{ $slot }}
+                </div>
+
+            </div>
+
+        </div>
+        </div>
+    </body>
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,144 @@
+@extends('layouts.home')
+{{-- section hero --}}
+@section('content')
+<section>
+    <div class="py-3 sm:py-32 bg-gradient-to-r from-cyan-300 to-blue-100">
+        <div>
+            <h1 class="pb-6 text-6xl text-center text-white">DISCOVER WORLD</h1>
+
+        </div>
+        <div class="px-6 py-20 mx-auto bg-black max-w-7xl lg:px-8">
+          <dl class="grid grid-cols-1 text-center gap-x-8 gap-y-16 lg:grid-cols-3">
+            <div class="flex flex-col max-w-xs mx-auto gap-y-4">
+              <dt class="text-base leading-7 text-white">Transactions every 24 hours</dt>
+              <dd class="order-first text-3xl font-semibold tracking-tight text-white sm:text-5xl">44 million</dd>
+            </div>
+            <div class="flex flex-col max-w-xs mx-auto gap-y-4">
+              <dt class="text-base leading-7 text-white">Assets under holding</dt>
+              <dd class="order-first text-3xl font-semibold tracking-tight text-white sm:text-5xl">$119 trillion</dd>
+            </div>
+            <div class="flex flex-col max-w-xs mx-auto gap-y-4">
+              <dt class="text-base leading-7 text-white ">New users annually</dt>
+              <dd class="order-first text-3xl font-semibold tracking-tight text-white sm:text-5xl">46,000</dd>
+            </div>
+          </dl>
+        </div>
+     </div>
+</section>
+{{-- cards --}}
+<section class="pt-10 lg:pt-[80px] pb-10 lg:pb-20 bg-[#F3F4F6]">
+    <div class="p-4" >
+     <h1 class="text-5xl font-bold tracking-tight text-center text-gray-900 sm:text-6xl"><em ></em> ADVENTURES <em class=" text-cyan-500"></em></h1>
+    </div>
+    <div class="container flex flex-col md:flex-row">
+          <div class="px-4 md:w-1/2 xl:w-1/3">
+             <div class="mb-10 overflow-hidden bg-white rounded-lg">
+                <img
+                   src="https://cdn.tailgrids.com/1.0/assets/images/cards/card-01/image-01.jpg"
+                   alt="image"
+                   class="w-full"
+                   />
+                <div class="p-8 text-center sm:p-9 md:p-7 xl:p-9">
+                   <h3
+
+                         href="javascript:void(0)"
+                         class="
+                         font-semibold
+                         text-dark text-xl
+                         sm:text-[22px]
+                         md:text-xl
+                         lg:text-[22px]
+                         xl:text-xl
+                         2xl:text-[22px]
+                         mb-4
+                         block
+                         hover:text-primary
+                         "
+                         >
+                Nom prenom
+
+                   </h3>
+                   <h4>Date de naissance : 12/12/2012</h4>
+                   <p class="text-base leading-relaxed text-body-color mb-7">
+                      Filiere : Genie info <br>
+                      <span>  Annee : 3eme</span>
+                   </p>
+
+                   <a
+                      href="javascript:void(0)"
+                      class="
+                      inline-block
+                      py-2
+                      px-7
+                      border border-[#2f4b82]
+                      rounded-full
+                      text-base text-body-color
+                      font-medium
+                      hover:border-primary hover:bg-primary hover:text-teal-800
+                      transition
+                      "
+                      >
+                        CHECK infos
+                   </a>
+                </div>
+             </div>
+          </div>
+
+    </div>
+ </section>
+@endsection
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <script>
+        const menubtn = document.getElementById('menu-button');
+        const menu = document.getElementById('menu');
+        menubtn.addEventListener('click', () => {
+            menu.classList.toggle('hidden');
+        });
+
+        const drpbtn = document.getElementById('drp-btn');
+        const drpList = document.getElementById('drp-list');
+        drpbtn.addEventListener('click', () => {
+            drpList.classList.toggle('hidden');
+        });
+    </script>
+
+
+
